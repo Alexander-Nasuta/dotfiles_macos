2c0d0e5a787466cdb0ae697cc8037137097e49c1
Alexander Nasuta <alexander.nasuta@ima.rwth-aachen.de>
2024-06-21 15:38:32 +0200

    added homebrew

---
+++ setup_homebrew.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+echo "\n <<<Starting Homebrew Setup >>> \n"
+
+
+# install homebrew as shown on its website (https://brew.sh)
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+# install via brewfile
+brew bundle --verbose
